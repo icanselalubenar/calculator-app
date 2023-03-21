0afaf382620e85f9e64d8455e1ed30f8f0c9049a
icanselalubenar <clashroyae1000@gmail.com>
2023-03-20 18:13:58 -0700

    inisialisasi project

---
+++ CalculatorAlpha.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Calculator
+{
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+
+	static void Main(string[] args)
+	{
+		int a = 10;
+		int b = 6;
+
+		Console.WriteLine("Hasil Penambahan : {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan : {0} - {1} = {2}", a, b, Pengurangan(a, b));
+
+		Console.WriteLine("Press a random key to exit.");
+		Console.ReadKey();
+	}
+}
